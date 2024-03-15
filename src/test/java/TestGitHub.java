937e75e9b00c46df043706c05c8490d18df550db
Pavel Kulikov <pkulikov@secureducation.ru>
2024-03-15 23:34:29 +0700

    add open url

--- src/test/java/TestGitHub.java
+++ src/test/java/TestGitHub.java
@@ -32,12 +32,14 @@ public class TestGitHub {
 
     @Test
     void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
         $("#column-a").shouldHave(text("B"));
     }
 
     @Test
     void dragAddDropToTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo($("#column-b"));
         $("#column-a").shouldHave(text("B"));
     }
